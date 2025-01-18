616ea916fcd68e8d774b0440937e67e67ebbbaaa
Vrozaksen <vrozaksen@protonmail.com>
2025-01-18 20:27:27 +0100

    feat(nut): final entrypoint with master/slave support

--- apps/network-ups-tools/entrypoint.sh
+++ apps/network-ups-tools/entrypoint.sh
@@ -2,10 +2,14 @@
 
 echo "*** NUT upsd startup ***"
 
-# initialize UPS driver
-printf "Starting up the UPS drivers ...\n"
-/usr/sbin/upsdrvctl -u root start || { printf "ERROR on driver startup.\n"; exit; }
+if [ -f /config/ups.conf ]; then
+  # Initialize UPS driver if ups.conf exists
+  printf "Starting up the UPS drivers ...\n"
+  /usr/sbin/upsdrvctl -u root start || { printf "ERROR on driver startup.\n"; exit 1; }
+else
+  printf "Skipping UPS driver startup. No ups.conf found.\n"
+fi
 
-# run the ups daemon
+# Run the UPS daemon
 printf "Starting up the UPS daemon ...\n"
-exec /usr/sbin/upsd -u root -D $* || { printf "ERROR on daemon startup.\n"; exit; }
+exec /usr/sbin/upsd -u root -D "$@" || { printf "ERROR on daemon startup.\n"; exit 1; }
