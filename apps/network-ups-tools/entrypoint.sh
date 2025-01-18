70b57a6945b4f65897270d83229b2d2543075aca
Vrozaksen <vrozaksen@protonmail.com>
2025-01-18 21:21:17 +0100

    feat(nut): different way to check driver

--- apps/network-ups-tools/entrypoint.sh
+++ apps/network-ups-tools/entrypoint.sh
@@ -3,9 +3,14 @@
 echo "*** NUT upsd startup ***"
 
 if [ -f /config/ups.conf ]; then
-  # Initialize UPS driver if ups.conf exists
-  printf "Starting up the UPS drivers ...\n"
-  /usr/sbin/upsdrvctl -u root start || { printf "ERROR on driver startup.\n"; exit 1; }
+  # Check if ups.conf contains "driver = none" or "driver=none"
+  if grep -qE '^\s*driver\s*=\s*none' /config/ups.conf; then
+    printf "Skipping UPS driver startup. 'driver = none' found in ups.conf.\n"
+  else
+    # Initialize UPS driver
+    printf "Starting up the UPS drivers ...\n"
+    /usr/sbin/upsdrvctl -u root start || { printf "ERROR on driver startup.\n"; exit 1; }
+  fi
 else
   printf "Skipping UPS driver startup. No ups.conf found.\n"
 fi
